262089d7f34e5550c100b79e58173987341fc08b
keiporras29 <kporras00988@ufide.ac.cr>
2025-12-11 10:43:16 -0600

    Realice la clase Cliente Agregue funciones

---
+++ src/Cliente.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Keisy
+ */
+public class Cliente {
+    private final String nombre;
+    private final String pasaporte;
+
+    public Cliente(String nombre, String pasaporte) {
+        this.nombre = nombre;
+        this.pasaporte = pasaporte;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPasaporte() {
+        return pasaporte;
+    }
+}
+
+
